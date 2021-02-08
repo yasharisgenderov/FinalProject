640f37b407960bcb8cbd0a4af7e0241f8a821ba8
Yaşar İsgəndərov <yasharisgenderov.0@gmail.com>
2021-02-08 18:20:37 +0400

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,62 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal : IProductDal
+    {
+        public void Add(Product entity)
+        {
+            using (NortwindContext context = new NortwindContext())
+            {
+                var addedcontex = context.Entry(entity);
+                addedcontex.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Product entity)
+        {
+            using (NortwindContext context = new NortwindContext())
+            {
+                var deletedcontex = context.Entry(entity);
+                deletedcontex.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            using (NortwindContext context = new NortwindContext())
+            {
+                return context.Set<Product>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            using (NortwindContext context = new NortwindContext())
+            {
+                return filter == null 
+                    ? context.Set<Product>().ToList() 
+                    : context.Set<Product>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(Product entity)
+        {
+            using (NortwindContext context = new NortwindContext())
+            {
+                var updatedcontex = context.Entry(entity);
+                updatedcontex.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/NortwindContext.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class NortwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb ; Database=Northwind : Trusted_Connection=true");
+        }
+        public DbSet<Product> products { get; set; }
+        public DbSet<Category> categories { get; set; }
+        public DbSet<Customer> customers { get; set; }
+    }
+}
+
