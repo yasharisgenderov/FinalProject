3cdb2ef0b7a6924a80862c346804d5565d74a9e7
Yaşar İsgəndərov <yasharisgenderov.0@gmail.com>
2021-03-26 23:59:12 +0400

    26 Mart

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
@@ -22,13 +23,10 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        [ValidationAspect(typeof(ProductValidator))] // add metodunu dogrula(Validation) ProductValidatordan istifade ederek
         public IResult Add(Product product) // utilities folderini void ucun istifadecini melumatlandirmaq ucun yaradiriq yeni resultlari bildirmek ucun.Daha sonra utilitylerin icinde IResult taratdim ve voidle deyisdim
         {
-            ValidationTool.Validate(new ProductValidator(), product);
-            //loglama
-            //cachremove
-            //performance
-            //transaction
+            /*ValidationTool.Validate(new ProductValidator(), product); bu kod ValidationAspectden evvel,onun yerine yazilmis bir koddur */
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
         }
---
+++ Core/Utilities/Interceptors/MethodInterceptionBaseAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Castle.DynamicProxy;
+using System;
+
+namespace Core.Utilities.Interceptors
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public abstract class MethodInterceptionBaseAttribute : Attribute, IInterceptor
+    {
+        public int Priority { get; set; } //birinci hansi atribut islesin onu gosterir tercumesi oncelik
+
+        public virtual void Intercept(IInvocation invocation)
+        {
+
+        }
+    }
+}
